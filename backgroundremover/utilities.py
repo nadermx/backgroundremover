a13e44a4071d94b847c631c262c3a825eb229c56
Johnathan Nader <john@nader.mx>
2023-01-21 17:06:00 -0600

    Update utilities.py

--- backgroundremover/utilities.py
+++ backgroundremover/utilities.py
@@ -307,36 +307,37 @@ def transparentvideooverimage(output, overlay, file_path,
     return
 
 def download_files_from_github(path, model_name):
-    if model_name == "u2net":
+    if model_name == "u2net" or model_name == "u2net_human_seg":
         part1 = tempfile.NamedTemporaryFile(delete=False)
         part2 = tempfile.NamedTemporaryFile(delete=False)
         part3 = tempfile.NamedTemporaryFile(delete=False)
         part4 = tempfile.NamedTemporaryFile(delete=False)
         try:
-            os.makedirs("~/.u2net")
+            os.makedirs(os.path.expanduser("~/.u2net"), exist_ok=True)
         except:
             print("u2net folder made or already exists")
         try:
+            url_base = 'https://github.com/nadermx/backgroundremover/raw/main/models/'
             print('download part1 of %s' % model_name)
-            part1_content = requests.get('https://github.com/nadermx/backgroundremover/raw/main/models/u2aa')
+            part1_content = requests.get(url_base + model_name[-1] + 'aa')
             part1.write(part1_content.content)
             part1.close()
             print('finished downloading part 1 of %s' % model_name)
             print('download part2 of %s' % model_name)
 
-            part2_content = requests.get('https://github.com/nadermx/backgroundremover/raw/main/models/u2ab')
+            part2_content = requests.get(url_base + model_name[-1] + 'ab')
             part2.write(part2_content.content)
             part2.close()
             print('finished downloading part 2 of %s' % model_name)
-            print('download part2 of %s' % model_name)
+            print('download part3 of %s' % model_name)
 
-            part3_content = requests.get('https://github.com/nadermx/backgroundremover/raw/main/models/u2ac')
+            part3_content = requests.get(url_base + model_name[-1] + 'ac')
             part3.write(part3_content.content)
             part3.close()
             print('finished downloading part 3 of %s' % model_name)
             print('download part4 of %s' % model_name)
 
-            part4_content = requests.get('https://github.com/nadermx/backgroundremover/raw/main/models/u2ad')
+            part4_content = requests.get(url_base + model_name[-1] + 'ad')
             part4.write(part4_content.content)
             part4.close()
             print('finished downloading part 4 of %s' % model_name)
@@ -347,51 +348,5 @@ def download_files_from_github(path, model_name):
             os.remove(part2.name)
             os.remove(part3.name)
             os.remove(part4.name)
-    if model_name == "u2net_human_seg":
-        part1 = tempfile.NamedTemporaryFile(delete=False)
-        part2 = tempfile.NamedTemporaryFile(delete=False)
-        part3 = tempfile.NamedTemporaryFile(delete=False)
-        part4 = tempfile.NamedTemporaryFile(delete=False)
-        try:
-            print('download part1 of %s' % model_name)
-            part1_content = requests.get('https://github.com/nadermx/backgroundremover/raw/main/models/u2haa')
-            part1.write(part1_content.content)
-            part1.close()
-            print('finished downloading part 1 of %s' % model_name)
-            print('download part2 of %s' % model_name)
-
-            part2_content = requests.get('https://github.com/nadermx/backgroundremover/raw/main/models/u2hab')
-            part2.write(part2_content.content)
-            part2.close()
-            print('finished downloading part 2 of %s' % model_name)
-            print('download part2 of %s' % model_name)
-
-            part3_content = requests.get('https://github.com/nadermx/backgroundremover/raw/main/models/u2hac')
-            part3.write(part3_content.content)
-            part3.close()
-            print('finished downloading part 3 of %s' % model_name)
-            print('download part4 of %s' % model_name)
-
-            part4_content = requests.get('https://github.com/nadermx/backgroundremover/raw/main/models/u2had')
-            part4.write(part4_content.content)
-            part4.close()
-            print('finished downloading part 4 of %s' % model_name)
-
-            sp.run(["cat", part1.name, part2.name, part3.name, part4.name, ">", path], stdout=sp.DEVNULL)
-        finally:
-            os.remove(part1.name)
-            os.remove(part2.name)
-            os.remove(part3.name)
-            os.remove(part4.name)
-
-    if model_name == "u2netp":
-        part1 = tempfile.NamedTemporaryFile(delete=False)
-        try:
-            print('download %s' % model_name)
-            part1_content = requests.get('https://github.com/nadermx/backgroundremover/raw/main/models/u2haa')
-            part1.write(part1_content.content)
-            part1.close()
-            print('finished downloading %s' % model_name)
-            sp.run(["cat", part1.name, ">", path], stdout=sp.DEVNULL)
-        finally:
-            os.remove(part1.name)
+    else:
+        print("Invalid model name")
