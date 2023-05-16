7724bdae5fcb2514c6885514124d0def2d1cc65d
nadermx <john@nader.mx>
2023-05-16 00:24:29 -0400

    Bump verion 0.2.2

--- backgroundremover/__init__.py
+++ backgroundremover/__init__.py
@@ -4,6 +4,6 @@ backgroundremover
 A library to remove background from videos and images
 """
 
-__version__ = "0.1.9"
+__version__ = "0.2.2"
 __author__ = 'Johnathan Nader'
-__credits__ = 'BackgroundRemover.app'
+__credits__ = 'BackgroundRemoverAI.com'
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="backgroundremover",
-    version="0.2.1",
+    version="0.2.2",
     description="Background remover from image and video",
     long_description=long_description,
     long_description_content_type="text/markdown",
