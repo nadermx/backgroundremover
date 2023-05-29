86d4c591a17b8bc99ca998073b7d5d12372c4be7
nadermx <john@nader.mx>
2023-05-28 19:32:55 -0600

    Bump verion 0.2.3.  Add in merge request which also closes https://github.com/nadermx/backgroundremover/issues/80

--- backgroundremover/__init__.py
+++ backgroundremover/__init__.py
@@ -4,6 +4,6 @@ backgroundremover
 A library to remove background from videos and images
 """
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 __author__ = 'Johnathan Nader'
 __credits__ = 'BackgroundRemoverAI.com'
